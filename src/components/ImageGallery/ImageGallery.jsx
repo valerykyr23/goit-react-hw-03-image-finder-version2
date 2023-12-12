2412bd4d3fc4bc066507091beb7d2520a3166eb1
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-12-11 22:46:26 -0800

    added imagegallery component  logic and loader component

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,51 +1,71 @@
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import React, { Component } from 'react';
-import { InfinitySpin } from 'react-loader-spinner';
+// import { InfinitySpin } from 'react-loader-spinner';
+import { Loader } from 'components/Loader/Loader';
 
 export class ImageGallery extends Component {
-
   state = {
-    imageInSearch: [],
-    loading: false,
+    imageInSearch: null,
     error: null,
+    status: 'idle'
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.imageInfo !== this.props.imageInfo) {
-      this.setState({ loading: true });
+      this.setState({ status: 'pending'});
       setTimeout(() => {
         fetch(
           `https://pixabay.com/api/?q=${this.props.imageInfo}&page=1&key=32855889-b519aab1920f45c88bc238e1c&image_type=photo&orientation=horizontal&per_page=12`
         )
-          .then(response => response.json())
-          .then(imageInSearch => this.setState({ imageInSearch: imageInSearch.hits }))
-          .catch(error => this.setState({ error }))
-          .finally(() => this.setState({ loading: false }));
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+
+            return Promise.reject(
+              new Error(
+                `Все сломалось, нет картинок по запросу {this.props.imageInfo}`
+              )
+            );
+          })
+
+          .then(imageInSearch =>
+            this.setState({ imageInSearch: imageInSearch.hits, status: 'resolved' })
+          )
+          .catch(error => this.setState({ error, status: 'rejected' }))
+          
       }, 2000);
     }
   }
 
-    render() {
-      
-        
+  render() {
 
-        return (
-          
-            
-        
-        <ul className={css.ImageGallery}>
-            
-                
-            { this.state.imageInSearch && this.state.imageInSearch.map(({ id, webformatURL }) => (<ImageGalleryItem id={id} smallImage={webformatURL} />))}
+    const { error, imageInSearch, status} = this.state;
+    
 
-            {this.state.loading && <InfinitySpin width="200" color="#4fa94d" />}
-            {!this.props.imageInfo && <div>Введите в поиск слово</div>}
-            { this.state.error && <div>Все сломалось</div> }
-         
-          
+    if (status === 'idle') {
+      return <div>Введите в поиск слово</div>
+    };
+
+    if (status === 'pending') {
+      return <Loader />
+    };
+
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>
+    }
+
+    if (status === 'resolved') {
+      return   <ul className={css.ImageGallery}>
+        
+         { imageInSearch.map(({ id, webformatURL }) => (<ImageGalleryItem id={id} smallImage={webformatURL} />)
+            
+          )}
+        
       </ul>
-    );
+    }
+
+    
   }
 }
-
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,6 @@
+import css from './Loader.module.css';
+
+export const Loader = () => {
+
+    return (<div className={css.loading}></div>)
+}
